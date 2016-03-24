aa85a8fe1769516dbec149a4209b511ad963bd22
Chaz Chandler <clc31@chaznet.local>
2016-03-24 01:46:59 +0000

    Added running collab server config

--- recipes/collab.rb
+++ recipes/collab.rb
@@ -8,26 +8,30 @@ require_recipe 'plunker::default'
 
 basedir     = node['plunker']['www_root']
 destdir     = File.join(basedir, 'plunker_collab')
+fqdn        = "#{node['hostname']}.#{node['dns']['domain']}"
+#fqdn        = 'localhost'
+conf_file   = '/etc/plunker/config.collab.json'
+task_name   = "plunker-collab"
 
-template '/etc/plunker/config.collab.json' do
+template conf_file do
   source 'config.collab.json.erb'
   owner 'root'
   group 'root'
   mode '0644'
   variables({
-    embed_server_fqdn: node.fqdn,
+    embed_server_fqdn: fqdn,
     embed_server_port: node['plunker']['embed']['port'],
-    www_server_fqdn: node.fqdn,
+    www_server_fqdn: fqdn,
     www_server_port: node['plunker']['www']['port'],
-    collab_server_fqdn: node.fqdn,
+    collab_server_fqdn: fqdn,
     collab_server_port: node['plunker']['collab']['port'],
-    api_server_fqdn: node.fqdn,
+    api_server_fqdn: fqdn,
     api_server_port: node['plunker']['api']['port'],
-    run_server_fqdn: node.fqdn,
+    run_server_fqdn: fqdn,
     run_server_port: node['plunker']['run']['port'],
+    protocol: node['plunker']['protocol'],
     github_client_id: node['plunker']['github']['client_id'],
-    github_client_secret: node['plunker']['github']['client_secret'],
-    protocol: node['plunker']['protocol']
+    github_client_secret: node['plunker']['github']['client_secret']
   })
   action :create
 end
@@ -56,6 +60,36 @@ tarball tmp_tb do
   not_if "test -d #{destdir}"
 end
 
-execute 'generate initd entry' do
-  command "initd-forever -a #{destdir}/server.js"
+execute 'install npm packages locally' do
+  command "npm install"
+  cwd destdir
+end
+
+link "#{destdir}/config.development.json" do
+  to conf_file
+end
+
+link "#{destdir}/config.production.json" do
+  to conf_file
+end
+
+#execute 'generate initd entry' do
+#  command "initd-forever -a #{destdir}/server.js && cp ./#{task_name} /etc/init.d/#{task_name} && chmod a+rx /etc/init.d/#{task_name}"
+#  cwd destdir
+#end
+
+template "/etc/init.d/#{task_name}" do
+  source 'forever-rc.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  variables({
+    task_name: task_name,
+    app_root:  destdir
+  })
+  action :create
+end
+
+service task_name do
+  action [:enable, :start]
 end
